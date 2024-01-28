d7bccf372701d546de2c91c62058636e0d5d3530
Seo jeong Kim <jennysujukim@gmail.com>
2024-01-28 14:53:44 -0800

    feat: adjust getProjectDataContext and apply to projects page

--- src/types/enums/BreakpointType.ts
+++
@@ -1,7 +0,0 @@
-export enum BreakpointType {
-  DesktopLg = 'desktop-lg',
-  Desktop = 'desktop',
-  Laptop = 'laptop',
-  Tablet = 'tablet',
-  Mobile = 'mobile',
-}
--- src/types/enums/DisciplineType.ts
+++
@@ -1,7 +0,0 @@
-export enum DisciplineType {
-  All = 'All',
-  Frontend = 'Frontend Development',
-  UIUX = 'UIUX Design',
-  Graphic = 'Graphic Design',
-  None = 'None',
-}
