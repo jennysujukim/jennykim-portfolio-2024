8088c09e4e67bbe4af82eb37cbb63b70e40489ff
Seo jeong Kim <jennysujukim@gmail.com>
2024-02-09 12:02:20 -0800

    chore: clean up Tab component code

--- src/components/atoms/Tab/Tab.tsx
+++ src/components/atoms/Tab/Tab.tsx
@@ -20,20 +20,46 @@ export default function Tab({
   return (
     <li className={isActive ? `${styles.Wrapper} ${styles.Active}` : styles.Wrapper}>
       {isLink &&
-        <a className={styles.Container} href={`#${href}`}>
-          {smallText && <span className={isActive ? `${styles.SmallText} ${styles.Active}` : styles.SmallText}>{smallText}</span>}
+        <a 
+          className={styles.Container_Home} 
+          href={`#${href}`}
+        >
+          {smallText && 
+            <span className={isActive ? `${styles.SmallText} ${styles.Active}` : styles.SmallText}>
+              {smallText}
+            </span>
+          }
           <div className={styles.Text_Cont}>
-            <p className={isActive ? `H5_Style ${styles.Text} ${styles.Active}` : `H5_Style ${styles.Text}`}>{text}</p>
-            {isActive && <p className={isActive ? `${styles.Bullet} ${styles.Active}` : styles.Bullet}>&bull;</p> }
+            <p className={isActive ? `H5_Style ${styles.Text_Home} ${styles.Active}` : `H5_Style ${styles.Text_Home}`}>
+              {text}
+            </p>
+            {isActive && 
+              <p className={isActive ? `${styles.Bullet} ${styles.Active}` : styles.Bullet}>
+                &bull;
+              </p> 
+            }
           </div>
         </a>
       }
       {!isLink && handleClick &&
-        <div className={styles.Container} onClick={() => handleClick(text)}>
-          {smallText && <span className={isActive ? `${styles.SmallText} ${styles.Active}` : styles.SmallText}>{smallText}</span>}
+        <div 
+          className={styles.Container} 
+          onClick={() => handleClick(text)}
+        >
+          {smallText && 
+            <span className={isActive ? `${styles.SmallText} ${styles.Active}` : styles.SmallText}>
+              {smallText}
+            </span>
+          }
           <div className={styles.Text_Cont}>
-            <p className={isActive ? `H5_Style ${styles.Text} ${styles.Active}` : `H5_Style ${styles.Text}`}>{text}</p>
-            {isActive && <p className={isActive ? `${styles.Bullet} ${styles.Active}` : styles.Bullet}>&bull;</p> }
+            <p className={isActive ? `H5_Style ${styles.Text} ${styles.Active}` : `H5_Style ${styles.Text}`}>
+              {text}
+            </p>
+            {isActive && 
+              <p className={isActive ? `${styles.Bullet} ${styles.Active}` : styles.Bullet}>
+                &bull;
+              </p>
+            }
           </div>
         </div>
       }
